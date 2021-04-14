27f740de3e207db400765ea2de4521a86dfa7df4
sonerkocak <soner.kocak@inavitas.com>
2021-04-14 10:47:29 +0300

    added _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,34 @@
-import '../styles/globals.css'
+import Router from "next/router";
+import ReactDOM from "react-dom";
+import '../styles/globals.css';
+import {delay} from '../common-util';
+
+Router.events.on("routeChangeStart", (url) => {
+  console.log(`Loading: ${url}`);
+  document.body.classList.add("body-page-transition");
+  ReactDOM.render(
+      <h1>{`Start ${url}`}</h1>,
+      document.getElementById("page-transition")
+  );
+});
+Router.events.on("routeChangeComplete", () => {
+  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
+  document.body.classList.remove("body-page-transition");
+});
+Router.events.on("routeChangeError", () => {
+  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
+  document.body.classList.remove("body-page-transition");
+});
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+MyApp.getInitialProps = async (ctx) => {
+  //await delay(3000);
+  return {
+    props: {},
+  };
+};
+
+export default MyApp;
